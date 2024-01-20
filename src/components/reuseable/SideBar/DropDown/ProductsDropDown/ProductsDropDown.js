8b83f62d09c7304a5301f73269e9ca210bae1192
Nayeem-Hasan <hasan@care-box.com>
2024-01-20 16:50:45 +0600

    ui changed

--- src/components/reuseable/SideBar/DropDown/ProductsDropDown/ProductsDropDown.js
+++ src/components/reuseable/SideBar/DropDown/ProductsDropDown/ProductsDropDown.js
@@ -1,16 +1,35 @@
+import { IoEllipseOutline } from "react-icons/io5";
+import { IoEllipse } from "react-icons/io5";
+
 function ProductsDropDown() {
   return (
     <div>
-      <div className="block text-left border-t py-3 border-red-200 text-base text-[#DEE4EE] font-normal leading-[19.09px] hover:bg-[#333A48] px-2 cursor-pointer">
-        Product
+      <div className="block text-left border-t py-3 border-red-200 text-base hover:text-green-600 px-2 cursor-pointer">
+        <div className="flex items-center gap-x-2">
+          <IoEllipseOutline />
+          Product List
+        </div>
       </div>
 
-      <div className="block text-left border-t py-3 border-red-200 text-base text-[#DEE4EE] font-normal leading-[19.09px] hover:bg-[#333A48] px-2 cursor-pointer">
-        Add Product
+      <div className="block text-left border-t py-3 border-red-200 text-base hover:text-green-600 px-2 cursor-pointer">
+        <div className="flex items-center gap-x-2">
+          <IoEllipse />
+          Add Product
+        </div>
       </div>
 
-      <div className="block text-left border-t py-3 border-red-200  text-base text-[#DEE4EE] font-normal leading-[19.09px] hover:bg-[#333A48] px-2 cursor-pointer">
-        Add Cetagory
+      <div className="block text-left border-t py-3 border-red-200 text-base hover:text-green-600 px-2 cursor-pointer">
+        <div className="flex items-center gap-x-2">
+          <IoEllipseOutline />
+          Category
+        </div>
+      </div>
+
+      <div className="block text-left border-t py-3 border-red-200 text-base hover:text-green-600 px-2 cursor-pointer">
+        <div className="flex items-center gap-x-2">
+          <IoEllipseOutline />
+          Sub Category
+        </div>
       </div>
     </div>
   );
--- src/components/reuseable/SideBar/SideBar.js
+++ src/components/reuseable/SideBar/SideBar.js
@@ -1,50 +1,48 @@
-'use client';
+"use client";
 
-import { useSelector } from 'react-redux';
-import './SideBar.css';
-import { RxDashboard } from 'react-icons/rx';
-import { RiShoppingCart2Fill } from 'react-icons/ri';
-import { BsBoxSeamFill } from 'react-icons/bs';
-import { MdOutlinePayment } from 'react-icons/md';
-import { FaUsers } from 'react-icons/fa';
-import { IoIosArrowDown } from 'react-icons/io';
-import ProductsDropDown from './DropDown/ProductsDropDown/ProductsDropDown';
-import { useState } from 'react';
+import { useSelector } from "react-redux";
+import "./SideBar.css";
+import { RxDashboard } from "react-icons/rx";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+import { BsBoxSeamFill } from "react-icons/bs";
+import { MdOutlinePayment } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+import ProductsDropDown from "./DropDown/ProductsDropDown/ProductsDropDown";
+import { useState } from "react";
 
 function SideBar() {
   const { status: SideBarStatus } = useSelector((state) => state.SideBar);
-  const [selectedDropdown, setSelectedDropdown] = useState('');
+  const [selectedDropdown, setSelectedDropdown] = useState("");
 
   const selectedDropdownHandler = (value) => {
     if (selectedDropdown !== value) {
       setSelectedDropdown(value);
     }
     if (selectedDropdown === value) {
-      setSelectedDropdown('');
+      setSelectedDropdown("");
     }
   };
 
   return (
     <div
       className={`${
-        SideBarStatus ? 'translate-x-0' : '-translate-x-full'
+        SideBarStatus ? "translate-x-0" : "-translate-x-full"
       } sidebar-container transition-all duration-200 ease-linear`}
     >
       <div className="mt-5">
-        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#DEE4EE] duration-300 ease-in-out hover:bg-[#333A48] cursor-pointer">
+        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[#333A48] hover:text-white cursor-pointer">
           <RxDashboard />
           Dashboard
         </div>
-
-        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#DEE4EE] duration-300 ease-in-out hover:bg-[#333A48] cursor-pointer">
+        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[#333A48] hover:text-white cursor-pointer">
           <RiShoppingCart2Fill />
           Orders
         </div>
-
         <div>
           <div
-            className="flex items-center justify-between gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#DEE4EE] duration-300 ease-in-out hover:bg-[#333A48] cursor-pointer"
-            onClick={() => selectedDropdownHandler('Products')}
+            className="flex items-center justify-between gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[#333A48] hover:text-white cursor-pointer"
+            onClick={() => selectedDropdownHandler("Products")}
           >
             <div className="flex items-center gap-4">
               <BsBoxSeamFill />
@@ -53,26 +51,36 @@ function SideBar() {
 
             <div
               className={`duration-300 ease-linear ${
-                selectedDropdown === 'Products' ? 'rotate-180' : 'rotate-0'
+                selectedDropdown === "Products" ? "rotate-180" : "rotate-0"
               }`}
             >
               <IoIosArrowDown />
             </div>
           </div>
 
-          {selectedDropdown === 'Products' && (
+          {selectedDropdown === "Products" && (
             <div className="px-4 ">
               <ProductsDropDown />
             </div>
           )}
         </div>
 
-        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#DEE4EE] duration-300 ease-in-out hover:bg-[#333A48] cursor-pointer">
+        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[#333A48] hover:text-white cursor-pointer">
           <MdOutlinePayment />
           Payment
         </div>
 
-        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#DEE4EE] duration-300 ease-in-out hover:bg-[#333A48] cursor-pointer">
+        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[#333A48] hover:text-white cursor-pointer">
+          <FaUsers />
+          Users
+        </div>
+
+        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[#1b2850] hover:text-white cursor-pointer">
+          <FaUsers />
+          Users
+        </div>
+
+        <div className="flex items-center gap-4 rounded-sm py-[11px] px-[15px] font-medium text-lg text-[#1B2850] duration-300 ease-in-out hover:bg-[hsl(220,17%,40%)] hover:text-white cursor-pointer">
           <FaUsers />
           Users
         </div>
